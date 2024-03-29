cf0f95d5fb76b47522a12fb1f3159b64aa4b33b0
Mohmammad Mahdi Arabi <mi1468@yahoo.com>
2024-03-29 17:14:17 +0330

    add requirements2

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-q1-xt8(+yr^6iye@sa3@miqn&(#-be96ild1s!o)wlmwqrzd3-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+# ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
